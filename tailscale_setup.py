9ccfa5a07306bcc59325f0dc578e2d0984ee87c8
Meka <82007659+pnmeka@users.noreply.github.com>
2023-07-16 16:57:36 -0400

    Update tailscale_setup.py

--- tailscale_setup.py
+++ tailscale_setup.py
@@ -23,3 +23,28 @@ subprocess.run(command1, check=True)
 os.system(command2)
 webbrowser.open(url)
 
+#write to darna.py
+
+file_path = f'{HS_path}/darna.py' 
+
+with open('flask1.py', 'r') as file:
+    lines = file.readlines()
+
+if len(lines) >= 3:
+    del lines[-3:]
+
+lines.append("\n@app.route('/tailscale')\n")
+lines.append("def tailscale():\n")
+lines.append("    if 'logged_in' in session:\n")
+lines.append("        # User is logged in, redirect to the protected page\n")
+lines.append("        url3 = f\"http://{ip_address}:8240\"\n")
+lines.append("        return redirect(url3)\n")
+lines.append("    else:\n")
+lines.append("        # User is not logged in, redirect to the login page\n")
+lines.append("        return redirect('/login')\n")
+lines.append("\nif __name__ == '__main__':\n")
+lines.append("    app.run('0.0.0.0', port=3001)\n")
+lines.append("    print(\"server is running at http://localhost:3001\")\n")
+
+with open('flask1.py', 'w') as file:
+    file.writelines(lines)
